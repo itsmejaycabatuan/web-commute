f3200125c7ebf5afdb7788a8de5c7c91586ae62d
itsmejaycabatuan <allenjaycabatuan@gmail.com>
2026-02-13 14:04:57 +0800

    first commit/added profile.blade.php or dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,39 @@
-<?php
+    <?php
 
-use Illuminate\Support\Facades\Route;
+    use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | Web Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register web routes for your application. These
+    | routes are loaded by the RouteServiceProvider within a group which
+    | contains the "web" middleware group. Now create something great!
+    |
+    */
 
-Route::get('/home', function () {
-    return view('home');
-})->name('home');
+    Route::get('/home', function () {
+        return view('home');
+    })->name('home');
 
 
-Route::get('/register', function () {
-    return view('register');
-})->name('register');
+    Route::get('/register', function () {
+        return view('register');
+    })->name('register');
 
-Route::get('/login',function (){
-    return view('login');
-})->name('login');
+    Route::get('/login',function (){
+        return view('login');
+    })->name('login');
 
-Route::get('/dashboard/commuter', function () {
-    return view('commuter.dashboard');
-})->name('commuter.dashboard');
+    Route::get('/dashboard/commuter', function () {
+        return view('commuter.dashboard');
+    })->name('commuter.dashboard');
 
-Route::get('/commuter/commuter', function () {
-    return view('commuter.commuter');
-})->name('commuter.commuter');
+    Route::get('/commuter/commuter', function () {
+        return view('commuter.commuter');
+    })->name('commuter.commuter');
+
+    Route::get('/profile', function () {
+        return view('profile');
+    })->name('profile');
